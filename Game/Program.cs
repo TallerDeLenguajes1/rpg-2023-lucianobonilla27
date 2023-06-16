0fbd4fa42d4b986e740221348c2023b13143b07f
lucianobonilla27 <113395398+lucianobonilla27@users.noreply.github.com>
2023-06-16 10:39:03 -0300

    Delete Game directory

--- Game/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using EspacioPersonaje;
-
-Personaje nuevo;
-FabricaDePersonaje fp = new();
-nuevo = fp.CrearPersonaje();
-Console.WriteLine(nuevo.Apodo);
